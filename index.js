56e913b62ac18feb6c6d77473101543d004ba6e8
Patrick dorgu <pat.dorgu@gmail.com>
2020-03-26 19:01:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
   const axios = require("axios");
@@ -47,54 +46,4 @@ function init() {
                 })
         })
 }
-=======
-
-
-  const axios = require("axios");
-const inquirer = require("inquirer");
-const generateHTML = require("./generateHTML");
-const PDF = require("html-pdf");
-const options = { format: "Letter" };
-const input = [
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Enter your GitHub username'
-    },
-    {
-        type: 'list',
-        name: 'favColor',
-        message: 'What is your favorite color?',
-        choices: [
-            'green',
-            'blue',
-            'pink',
-            'red',
-            'orange',
-            'purple'
-        ]
-    }
-];
-function prompt() {
-    return inquirer.prompt(input);
-}
-function init() {
-    prompt()
-        .then(({ username, favoriteColor }) => {
-            const queryURL = `https://api.github.com/users/${username}`;
-            axios.get(queryURL)
-                .then(res => {
-                    res.data.color = favoriteColor;
-                    const html = generateHTML(res.data);
-                    PDF.create(html, options).toFile(`./${res.data.name}.pdf`, (err, res) => {
-                        if (err) {
-                            return console.log(err);
-                        } else {
-                            console.log(res);
-                        }
-                    });
-                })
-        })
-}
->>>>>>> e36bf408025fdc7243e27a6babfe3bf96978be33
 init();
